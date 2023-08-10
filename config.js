1ed404a5f17e024586b56940fc6b41bb6d879797
NightsFall <125892967+NightsFall@users.noreply.github.com>
2023-08-10 19:49:23 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+window.__CONFIG__ = {
+  // url must NOT end with a slash
+  VITE_CORS_PROXY_URL: "https://streamer-proxy.milan-uses.workers.dev",
+  VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyYTY3ODFkNjE1MDg2MGI3YmFhNjlkMWY3OWUzMmI1YSIsInN1YiI6IjY0YTI4ODY2ODI4OWEwMDEwY2VjNjYzMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.D-ZrIzZVji5hrY7ZEXRUpgQZfSyW06562ofBScEjzs4"
+};
